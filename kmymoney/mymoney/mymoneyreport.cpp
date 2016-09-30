ba35fc474fec665c7a58e7a8b2c2cd4e50f5b695
Ralf Habacker <ralf.habacker@freenet.de>
2016-09-30 14:01:27 +0200

    Add equity accounts to asset and liability reports.

--- kmymoney/mymoney/mymoneyreport.cpp
+++ kmymoney/mymoney/mymoneyreport.cpp
@@ -144,6 +144,7 @@ MyMoneyReport::MyMoneyReport(ERowType _rt, unsigned _ct, dateOptionE _dl, EDetai
   if (_rt == MyMoneyReport::eAssetLiability) {
     addAccountGroup(MyMoneyAccount::Asset);
     addAccountGroup(MyMoneyAccount::Liability);
+    addAccountGroup(MyMoneyAccount::Equity);
     m_showRowTotals = true;
   }
   if (_rt == MyMoneyReport::eExpenseIncome) {
@@ -242,6 +243,7 @@ void MyMoneyReport::setRowType(ERowType _rt)
   if (_rt == MyMoneyReport::eAssetLiability) {
     addAccountGroup(MyMoneyAccount::Asset);
     addAccountGroup(MyMoneyAccount::Liability);
+    addAccountGroup(MyMoneyAccount::Equity);
   }
   if (_rt == MyMoneyReport::eExpenseIncome) {
     addAccountGroup(MyMoneyAccount::Expense);
