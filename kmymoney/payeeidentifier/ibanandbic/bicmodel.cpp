8f8f3cf9f814b40fc5f5b43e4fd21c35c1b551ea
Ralf Habacker <ralf.habacker@freenet.de>
2019-03-04 08:03:58 +0100

    Use ibanbicdata from ktoblzcheck

--- kmymoney/payeeidentifier/ibanandbic/bicmodel.cpp
+++ kmymoney/payeeidentifier/ibanandbic/bicmodel.cpp
@@ -76,7 +76,7 @@ bicModel::bicModel(QObject* parent)
     QString database = service->property(QLatin1String("X-KMyMoney-Bankdata-Database")).toString();
 
     // Locate database
-    QString path = KGlobal::dirs()->locate("data", QLatin1String("kmymoney/ibanbicdata/") + database);
+    QString path = KGlobal::dirs()->locate("data", QLatin1String("ktoblzcheck/ibanbicdata/") + database);
     if (path.isEmpty()) {
       kWarning() << QString("Could not locate database file \"%1\" to receive BIC data.").arg(database);
     } else {
