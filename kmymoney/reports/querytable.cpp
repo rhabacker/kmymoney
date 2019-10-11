321fb0aaa9fc2aa05866b3e4893f98166be1b28d
Ralf Habacker <ralf.habacker@freenet.de>
2019-10-11 20:07:53 +0200

    Unify usage of include_me in QueryTable::constructTransactionTable()

--- kmymoney/reports/querytable.cpp
+++ kmymoney/reports/querytable.cpp
@@ -361,6 +361,7 @@ void QueryTable::constructTransactionTable()
     do {
       MyMoneyMoney xr;
       ReportAccount splitAcc = (* it_split).accountId();
+      include_me = m_config.includes(splitAcc);
 
       //get fraction for account
       int fraction = splitAcc.currency().smallestAccountFraction();
@@ -399,7 +400,6 @@ void QueryTable::constructTransactionTable()
 
       if (it_split == myBegin) {
 
-        include_me = m_config.includes(splitAcc);
         a_fullname = splitAcc.fullName();
         a_memo = (*it_split).memo();
 
@@ -570,7 +570,7 @@ void QueryTable::constructTransactionTable()
               qA ["categorytype"] = KMyMoneyUtils::accountTypeToString(splitAcc.accountGroup());
             }
 
-            if (use_transfers || (splitAcc.isIncomeExpense() && m_config.includes(splitAcc))) {
+            if (use_transfers || (splitAcc.isIncomeExpense() && include_me)) {
               //if it matches the text of the main split of the transaction or
               //it matches this particular split, include it
               //otherwise, skip it
@@ -597,7 +597,7 @@ void QueryTable::constructTransactionTable()
           }
         }
 
-        if (m_config.includes(splitAcc) && use_transfers) {
+        if (include_me && use_transfers) {
           if (! splitAcc.isIncomeExpense()) {
             //multiply by currency and convert to lowest fraction
             qS["value"] = ((*it_split).shares() * xr).convert(fraction).toString();
