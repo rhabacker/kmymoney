7401699b88c480724927ad7a80f4442e75e7ce42
Ralf Habacker <ralf.habacker@freenet.de>
2024-02-06 16:27:21 +0100

    Show payee link in transaction editor/form

--- kmymoney/views/newtransactioneditor.cpp
+++ kmymoney/views/newtransactioneditor.cpp
@@ -144,6 +144,7 @@ public:
     MyMoneySplit m_split;
     WidgetHintFrameCollection* frameCollection;
     KMyMoneyAccountComboSplitHelper* m_splitHelper;
+    void updateMemoLink();
 };
 
 void NewTransactionEditor::Private::adjustTagIdList()
@@ -975,6 +976,8 @@ void NewTransactionEditor::Private::loadTransaction(QModelIndex idx)
 
             ui->numberEdit->setText(splitIdx.data(eMyMoney::Model::SplitNumberRole).toString());
             ui->statusCombo->setCurrentIndex(splitIdx.data(eMyMoney::Model::SplitReconcileFlagRole).toInt());
+            ui->tagContainer->loadTags(splitIdx.data(eMyMoney::Model::SplitTagIdRole).toStringList());
+            updateMemoLink();
         } else {
             splitModel.appendSplit(MyMoneyFile::instance()->journalModel()->itemByIndex(splitIdx).split());
 
@@ -1007,6 +1010,25 @@ void NewTransactionEditor::Private::loadTransaction(QModelIndex idx)
 
     updateWidgetState();
     m_splitHelper->updateWidget();
+}
+
+void NewTransactionEditor::Private::updateMemoLink()
+{
+    try {
+        const MyMoneyPayee& payeeObj = MyMoneyFile::instance()->payeeByName(ui->payeeEdit->currentText());
+        QUrl url = payeeObj.payeeLink(ui->memoEdit->toPlainText());
+        if (url.isEmpty()) {
+            ui->linkLabel->setText("");
+            return;
+        }
+        ui->linkLabel->setTextFormat(Qt::RichText);
+        ui->linkLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
+        ui->linkLabel->setOpenExternalLinks(true);
+        ui->linkLabel->setText(QString("<a href=\"%1\">%2</a>").arg(url.toString(), i18n("Link")));
+        qDebug() << url;
+    } catch (MyMoneyException&) {
+        ui->linkLabel->setText("");
+    }
 }
 
 NewTransactionEditor::NewTransactionEditor(QWidget* parent, const QString& accountId)
@@ -1272,6 +1294,8 @@ void NewTransactionEditor::loadSchedule(const MyMoneySchedule& schedule)
 
                 d->ui->numberEdit->setText(split.number());
                 d->ui->statusCombo->setCurrentIndex(static_cast<int>(split.reconcileFlag()));
+                d->ui->tagContainer->loadTags(split.tagIdList());
+                d->updateMemoLink();
             } else {
                 // we block sending out signals for the category combo here to avoid
                 // calling NewTransactionEditorPrivate::categoryChanged which does not
