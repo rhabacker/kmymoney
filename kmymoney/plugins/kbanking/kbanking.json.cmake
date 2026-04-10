0a65e993c6eb14675910d1bb912c26f774964356
l10n daemon script <scripty@kde.org>
2026-04-10 01:54:59 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- kmymoney/plugins/kbanking/kbanking.json.cmake
+++ kmymoney/plugins/kbanking/kbanking.json.cmake
@@ -56,6 +56,7 @@
         "Description[pt]": "Adição do HBCI/FinTs, OFX e banca 'online' do PayPal ao KMyMoney",
         "Description[pt_BR]": "Adiciona correspondente bancário do tipo HBCI/FinTs, OFX e PayPal no KMyMoney",
         "Description[ru]": "Добавляет в KMyMoney возможность сетевой работы с банками (HBCI/FinTs, OFX и PayPal)",
+        "Description[sk]": "Pridať HBCI/FinTs online banking do KMyMoney",
         "Description[sl]": "Doda HBCI/FinTs, OFX and PayPal spletno bančništvo v KMyMoney",
         "Description[sv]": "Lägg till HBCI/FinTs Internetbank och PayPal till KMyMoney",
         "Description[tr]": "K Param’a HBCI/FinTs, OFX ve PayPal çevrim içi bankacılığını ekleyin",
